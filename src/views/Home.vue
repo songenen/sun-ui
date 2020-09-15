79fc080f25e179bb8deaa5a624e0b1ce36ca6423
Songsong <614750803@qq.com>
2020-09-15 15:29:56 +0800

    aside html 初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>bhc-ui</h1>
       <h2>很棒的UI框架</h2>
       <div class="actions">
-        <a href="">GitHub</a>
-        <a href="">开始</a>
+        <a href="https://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </div>
     </div>
   </div>
