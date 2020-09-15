2087e39a5170128af92d3d85d4fd871ec0e3dd98
Songsong <614750803@qq.com>
2020-09-15 14:50:57 +0800

    搭建html模块

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>bhc-ui</h1>
+      <h2>很棒的UI框架</h2>
+      <div class="actions">
+        <a href="">GitHub</a>
+        <a href="">开始</a>
+      </div>
+    </div>
+  </div>
+</template>
