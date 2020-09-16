8e36504aeb47cc033c886b3625c0d0569d2babd1
Songsong <614750803@qq.com>
2020-09-16 16:00:47 +0800

    创建button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,8 +1,17 @@
 <template>
-<div> button</div>
+<div> button
+<h1>2222222</h1>
+  <div>
+    <Button>nihao</Button>
+  </div>
+</div>
 </template>
 
 <script lang="ts">
+import Button from '../lib/Button.vue';
+export default {
+  components:{Button}
+}
 </script>
 
 <style lang="scss" scoped>
