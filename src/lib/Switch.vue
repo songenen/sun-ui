d56f2138e9e5d94febe55a2ba60f4d9da12bb4f4
Songsong <614750803@qq.com>
2020-09-17 23:14:01 +0800

    css最小影响原则

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,5 @@
 <template>
-  <button @click="toggle" :class="{checked:value}"><span></span></button>
+  <button class="gulu-switch" @click="toggle" :class="{'gulu-checked':value}"><span></span></button>
 </template>
 
 <script lang="ts">
@@ -18,17 +18,17 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
 $h: 22px;
 $h2: $h - 4px;
-button {
+.gulu-switch {
   height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
 
   > span {
     position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;
   }
 
-  &.checked { background: #1890ff;
+  &.gulu-checked { background: #1890ff;
 
     > span { left: calc(100% - #{$h2} - 2px); }
   }
@@ -39,7 +39,7 @@ button {
     > span { width: $h2 + 4px; }
   }
 
-  &.checked:active {
+  &.gulu-checked:active {
     > span { width: $h2 + 4px; margin-left: -4px; }
   }
 }
