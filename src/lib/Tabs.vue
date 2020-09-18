8d6327ccbf53c0e4f12f3567677d7ed703d57e88
Songsong <614750803@qq.com>
2020-09-18 22:17:07 +0800

    完成Tabs组件

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -6,13 +6,13 @@
 
     </div>
     <div class="gulu-tabs-content">
-      <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
+      <component :is="current" :key="current.props.title" />
     </div>
   </div>
 </template>
 <script lang="ts">
 import Tab from './Tab.vue'
-import {computed, ref, onMounted, onUpdated} from 'vue'
+import {computed, ref, onMounted, onUpdated,watchEffect} from 'vue'
 export default {
   props: {
     selected: {
@@ -23,22 +23,22 @@ export default {
     const selectedItem = ref < HTMLDivElement > (null)
     const indicator = ref < HTMLDivElement > (null)
     const container = ref < HTMLDivElement > (null)
-    const x = () => {
-      const {
-        width
-      } = selectedItem.value.getBoundingClientRect()
-      indicator.value.style.width = width + 'px'
-      const {
-        left: left1
-      } = container.value.getBoundingClientRect()
-      const {
-        left: left2
-      } = selectedItem.value.getBoundingClientRect()
-      const left = left2 - left1
-      indicator.value.style.left = left + 'px'
-    }
-    onMounted(x)
-    onUpdated(x)
+    onMounted(() => {
+      watchEffect(() => {
+        const {
+          width
+        } = selectedItem.value.getBoundingClientRect()
+        indicator.value.style.width = width + 'px'
+        const {
+          left: left1
+        } = container.value.getBoundingClientRect()
+        const {
+          left: left2
+        } = selectedItem.value.getBoundingClientRect()
+        const left = left2 - left1
+        indicator.value.style.left = left + 'px'
+      })
+    })
 
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
@@ -46,7 +46,9 @@ export default {
         throw new Error('Tabs 子标签必须是 Tab')
       }
     })
-
+    const current = computed(() => {
+      return defaults.find(tag => tag.props.title === props.selected)
+    })
     const titles = defaults.map((tag) => {
       return tag.props.title
     })
@@ -54,6 +56,7 @@ export default {
       context.emit('update:selected', title)
     }
     return {
+      current,
       defaults,
       titles,
       select,
@@ -94,17 +97,10 @@ $border-color: #d9d9d9;
       bottom: -1px;
       width: 100px;
       transition: all 250ms;
-
     }
   }
   &-content {
     padding: 8px 0;
-    &-item {
-      display: none;
-      &.selected {
-        display: block;
-      }
-    }
   }
 }
 </style>
