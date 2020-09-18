9fd7056a5a85987eaf53d5d36d6a18f503d2c473
Songsong <614750803@qq.com>
2020-09-18 19:57:55 +0800

    添加 Tab和Tabs + css

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,8 +1,20 @@
 <template>
-<div>tabs</div>
+  <div>Tabs 示例</div>
+  <h1>示例1</h1>
+  <Tabs>
+    <Tab title="导航1">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
+  </Tabs>
 </template>
 
 <script lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+export default {
+  components:{
+    Tabs,Tab
+  }
+}
 </script>
 
 <style lang="scss" scoped>
--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -63,7 +63,7 @@ export default {
     }
   }
   const cancel = () => {
-    context.emit('cancel')
+    props.cancel?.()
     close()
   }
   return {
