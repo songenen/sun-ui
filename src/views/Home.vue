b18f69dbc38e9039c0e494bf95def79ec1e2ce7a
Songsong <614750803@qq.com>
2020-09-19 19:57:50 +0800

    添加圆弧

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -38,6 +38,8 @@ $border-radius: 4px;
 $color: #007974;
 .topnavAndBanner {
   background: linear-gradient(145deg, rgba(227, 255, 253, 1) 0%, rgba(183, 233, 230, 1) 100%);
+  clip-path: ellipse(100% 60% at 50% 40%);
+
 }
 .features {
   >svg {
