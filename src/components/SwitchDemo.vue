49a784fda69d9733d5a8d3d3dad13a4b3e37bd04
Songsong <614750803@qq.com>
2020-09-20 21:54:29 +0800

    源代码添加语言颜色和背景

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{Switch1Demo.__sourceCode}}</pre>
+        <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')" />
       </div>
       <div class="demo">
         <h2>支持 disabled </h2>
@@ -21,7 +21,7 @@
           <Button>查看代码</Button>
         </div>
         <div class="demo-code">
-          <pre>{{Switch2Demo.__sourceCode}}</pre>
+          <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')" />
         </div>
       </div>
     </div>
@@ -33,6 +33,9 @@ import Switch from '../lib/Switch.vue'
 import Button from '../lib/Button.vue'
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
+import 'prismjs';
+import 'prismjs/themes/prism-okaidia.css'
+const Prism = (window as any).Prism
 import { ref } from 'vue'
 
 export default {
@@ -41,7 +44,8 @@ export default {
     const bool = ref(false)
     return {bool,
       Switch1Demo,
-      Switch2Demo}
+      Switch2Demo,
+      Prism}
   }
 }
 </script>
