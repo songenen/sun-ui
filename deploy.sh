b63cdd7f97bb672c8183ca39e92f9e6acb2f3c3b
Songsong <614750803@qq.com>
2020-09-25 15:06:34 +0800

    使用script setup

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ git init &&
 git add . &&
 git commit -m "update" &&
 git branch -M master &&
-git remote add origin git@github.com:songenen/sun-ui-1.git &&
+git remote add origin git@gitee.com:songenn/sun-ui.git &&
 git push -f -u origin master &&
 cd -
 echo https://songenen.github.io/sun-ui-1/index.html
